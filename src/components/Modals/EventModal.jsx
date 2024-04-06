a872b96bfbee8075ad5ff1c040d29a1fbd8cf422
Erniel <ninelcaalim12@gmail.com>
2024-04-06 09:34:02 +0800

    Feat: Added drop down list for name and event

--- src/components/Modals/EventModal.jsx
+++ src/components/Modals/EventModal.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 const EventModal = () => {
+  const [isOpenName, setIsOpenName] = useState(false);
+  const [isOpenEvent, setIsOpenEvent] = useState(false);
   return (
     <div className="absolute z-10 flex justify-center w-screen h-screen bg-black bg-opacity-25">
-      <div className="w-[60vw] overflow-y-scroll h-auto mt-[10vh] bg-white rounded-md shadow gap-6 flex flex-col px-10 py-5">
+      <div className="w-[50vw] overflow-y-scroll h-fit mt-[10vh] bg-white rounded-md shadow gap-6 flex flex-col px-10 py-5">
         <h1 className="mb-4 text-xl font-medium text-gray-500 font-roboto">
           Lending Form
         </h1>
-        <div className="flex flex-row w-auto gap-10">
+        <div className="flex flex-row w-auto gap-10 ">
           <div className="flex flex-row items-center justify-start h-10 gap-4">
             <label
               className="pl-2 text-sm font-medium text-gray-500 whitespace-nowrap font-roboto"
@@ -66,33 +70,89 @@ const EventModal = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col gap-2">
-          <label
-            className="pl-2 text-sm text-gray-500 font-roboto focus:ring-1"
-            htmlFor="name"
-          >
-            Name: <span className="text-red-500">*</span>
-          </label>
-          <input
-            className="h-10 p-2 text-sm text-gray-500 border rounded outline-none font-roboto focus:ring-1"
-            type="text"
-            id="name"
-            placeholder="Enter your name"
-          />
+        <div className="relative">
+          <div className="flex flex-col gap-2">
+            <label
+              className="pl-2 text-sm text-gray-500 font-roboto focus:ring-1"
+              htmlFor="name"
+            >
+              Name: <span className="text-red-500">*</span>
+            </label>
+            <input
+              onClick={() => setIsOpenName(!isOpenName)}
+              className="h-10 p-2 text-sm text-gray-500 border rounded outline-none font-roboto focus:ring-1"
+              type="text"
+              id="name"
+              placeholder="Enter your name"
+            />
+          </div>
+          {isOpenName && (
+            <ul className="absolute w-full h-auto mt-[1px] bg-white border rounded-md shadow-md overflow-y-scroll max-h-[200px] z-10">
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+            </ul>
+          )}
         </div>
-        <div className="flex flex-col gap-2">
-          <label
-            className="pl-2 text-sm text-gray-500 font-roboto"
-            htmlFor="event"
-          >
-            Event:
-          </label>
-          <input
-            className="h-10 p-2 text-sm text-gray-500 border rounded outline-none font-roboto focus:ring-1"
-            type="text"
-            id="event"
-            placeholder="Enter the name of event"
-          />
+        <div className="relative">
+          <div className="flex flex-col gap-2">
+            <label
+              className="pl-2 text-sm text-gray-500 font-roboto"
+              htmlFor="event"
+            >
+              Event:
+            </label>
+            <input
+              onClick={() => setIsOpenEvent(!isOpenEvent)}
+              className="h-10 p-2 text-sm text-gray-500 border rounded outline-none font-roboto focus:ring-1"
+              type="text"
+              id="event"
+              placeholder="Enter event name"
+            />
+          </div>
+          {isOpenEvent && (
+            <ul className="absolute w-full h-auto mt-[1px] bg-white border rounded-md shadow-md overflow-y-scroll max-h-[200px]">
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+              <li className="h-10 px-6 py-2 text-sm text-gray-500 border-b font-roboto hover:bg-gray-100">
+                adasdas
+              </li>
+            </ul>
+          )}
         </div>
 
         <div className="flex flex-col gap-2">
