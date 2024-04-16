009f55c43fc31beee54584d43342e93449a5748b
Erniel <ninelcaalim12@gmail.com>
2024-04-16 10:35:14 +0800

    Feat: added github page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/office-device-lending-app/',
   server: {
     proxy: {
       "/api": "http://localhost:5000",
